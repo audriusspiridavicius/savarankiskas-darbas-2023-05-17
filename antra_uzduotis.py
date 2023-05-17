e338a743a3cbb02f4cefe73704589e8e1331176a
unknown <audriusspiridavicius@gmail.com>
2023-05-17 18:33:52 +0300

    antra uzduotis padaryta

---
+++ antra_uzduotis.py
@@ -0,0 +1,6 @@
+from list_functions import get_unique_records
+data = (1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2)
+data2 = ("a", "b", "e", "a")
+
+print(get_unique_records(data))
+print(get_unique_records(data2))
--- list_functions.py
+++ list_functions.py
@@ -2,3 +2,10 @@ def get_word_first_letter(words):
     letters = [word[0].upper() for word in words]
 
     return letters
+
+
+def get_unique_records(values):
+
+    unique_values = set(values)
+
+    return tuple(unique_values)
