e79a0d0a8cc1fb871d77a03d3109490d19cc4c08
unknown <audriusspiridavicius@gmail.com>
2023-05-17 18:25:20 +0300

    pirma uzduotis padaryta

---
+++ list_functions.py
@@ -0,0 +1,4 @@
+def get_word_first_letter(words):
+    letters = [word[0].upper() for word in words]
+
+    return letters
---
+++ pirma_uzduotis.py
@@ -0,0 +1,7 @@
+from list_functions import *
+
+name_list = ["Audrius", "Dovydas", "jonas", "PETRAS", "kaZys"]
+
+letters_list = get_word_first_letter(name_list)
+print(letters_list)
+
